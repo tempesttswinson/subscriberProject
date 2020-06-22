9aec3730e626a0489c02824ca84b1279e9d00b5a
tempesttswinson <tempestt.swinson@gmail.com>
2020-06-22 08:38:11 -0400

    created new Spring Boot project using Maven that will create a list of subscribers to a database

---
+++ src/test/java/com/win/subscriber_list/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.win.subscriber_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
